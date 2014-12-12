8b62135709ad1f304e77e25c9a2314444c1d1754
Teddy <Teddy@n184-p105.kthopen.kth.se>
2014-12-12 14:42:30 +0100

    Added database to chat

--- WebsocketServers/Node/websocket.js
+++ WebsocketServers/Node/websocket.js
@@ -1,38 +1,45 @@
+var WebSocketServer = require('ws').Server
+var wss = new WebSocketServer({port: 8090});
+var mysql = require('mysql');
 var clients = [];
 var clientNames = [];
-
-var WebSocketServer = require('ws').Server
-  , wss = new WebSocketServer({port: 8090});
+var pool  = mysql.createPool({
+  connectionLimit : 10,
+  host     : '130.237.84.126',
+  user     : 'root',
+  password : 'ninjakick',
+  database : 'socialnetwork'
+});
 
 wss.on('connection', function(ws) {
     
     //Do the first connection stuff here add client ad such
-    ws.send('Connected');
+    //ws.send('Connected');
 
-	//What to do when we get a message(check with db and send message)
+    var user = (ws.upgradeReq.url).substring(1);
+    console.log("Socket is now open + " + user)
+
+    if (!(clients.indexOf(ws) > -1)) {
+        console.log("Adding client nr: " +clients.length);
+        clients.push(ws);
+        clientNames.push(user);
+        ws.send("Connected: "+user);
+    }
+
     ws.on('message', function(message) {
 		var json = JSON.parse(message);
+        var query = 'call newMessage('+ pool.escape(json.sender) + ',' + pool.escape(json.reciver)+ ',' +pool.escape(json.message) + ')';
+        pool.query(query, function(err, rows, fields) {
+        if (err) throw err;
 
-		if (clients.indexOf(ws) > -1) {
-    	//In the array!
-         //broadcast(message);
-        sendTo(json.reciver, json.message)
+        if(rows[0][0].success == 1){
+            sendTo(json.reciver, message, ws);
+        }
 
-		} else {
-    	//Not in the array adding client
-    	console.log("Adding client nr: " +clients.length);
-    	clients.push(ws);
-    	clientNames.push(json.sender);
-
-        ws.send("You have been connected!");
-         //broadcast(json.message);
-        sendTo(json.reciver, json.message)
-
-		}	
+        });
 
     });
 
-    //What to do when a socket is closed(remove client and such)
     ws.on('close', function(message) {
         console.log('connection was closed ' + message);
         var index = clients.indexOf(ws);
@@ -44,29 +51,20 @@ wss.on('connection', function(ws) {
 
 });
 
-function broadcast(message) {
-    for (var i = 0; i < clients.length; i++) {
-        client = clients[i];
-        client.send(message);
-    }
-}
-
-function sendTo(reciver, message){
+function sendTo(reciver, message, senderSocket){
 
 	console.log("Trying to send to: " + reciver);
 	console.log("Connected: " + clientNames.toString());
 
-
 	var reciverId = clientNames.indexOf(reciver);
 		if (reciverId > -1) {
 			clients[reciverId].send(message);
+            senderSocket.send(message);
+
 		}else{
-			console.log("No client with that name");
+			console.log("No client with that name ");
+            senderSocket.send(message);
+
 		}
-}
-
-function addToDB(){
-
-	
 }
 
